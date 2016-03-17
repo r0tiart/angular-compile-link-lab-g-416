5b66fcdb3a96eb9e5cac8659d02ac0f4ada338cd
Todd Motto <todd@toddmotto.com>
2016-03-17 11:58:10 +0000

    chore: Added solution

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -2,11 +2,20 @@ function SomeDirective() {
 	return {
 		template: [
 			'<div>',
-				'Replace this text!',
+			'Replace this text!',
 			'</div>'
 		].join(''),
-		link: function (scope, elem, attrs) {
-
+		compile: function (element) {
+			return {
+				pre: function (scope, element, attrs) {
+					element[0].innerText = 'Changed text';
+				},
+				post: function (scope, element, attrs) {
+					element[0].addEventListener('click', function () {
+						alert('Hey!');
+					});
+				}
+			}
 		}
 	}
 }
