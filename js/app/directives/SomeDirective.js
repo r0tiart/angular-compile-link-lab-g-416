0bd2cdae02a9fb6ca4cc4e0ec98293b05ad4a6d9
r0tiart <antley.chu@gmail.com>
2018-03-18 20:36:35 -0400

    Done.

--- js/app/directives/SomeDirective.js
+++ js/app/directives/SomeDirective.js
@@ -7,6 +7,19 @@ function SomeDirective() {
 		].join(''),
 		link: function (scope, elem, attrs) {
 
+		},
+		compile: function(){
+
+			return{
+				pre: function(scope,element,attrs){
+					element[0].innerText = "another text"
+				},
+				post: function(scope, element, attrs){
+					element[0].addEventListener('click',function(){
+						alert("I've been clicked!")
+					})
+				}
+			}
 		}
 	}
 }
